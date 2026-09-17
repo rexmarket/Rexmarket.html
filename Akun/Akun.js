9a6dad0b20841bdb8cbc64c9338347db84ff12fc
rexmarket <azilkhtr@gmail.com>
2026-09-17 15:46:07 +0700

    Update redirect path for Rexmarket.html

--- Akun/Akun.js
+++ Akun/Akun.js
@@ -82,8 +82,7 @@ googleBtn.addEventListener("click", async () => {
     `Login berhasil 👋 Selamat datang, ${user.displayName || "User"}!`
 );
 
-window.location.href = "Rexmarket.html";
-
+window.location.href = "../Rexmarket.html";
 
     } catch (error) {
 
