acd4655e6725d0ebd9d7245c1e20857271f55b9d
rexmarket <azilkhtr@gmail.com>
2026-09-17 14:19:56 +0700

    Set up Firebase configuration and initialization
    Added Firebase configuration and initialization code.

--- Akun/Firebase.js
+++ Akun/Firebase.js
@@ -1,1 +1,68 @@
+// =========================================
+// FIREBASE CONFIGURATION
+// =========================================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// =========================================
+// FIREBASE CONFIG
+// =========================================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBJuILOgk3eP6pTXIEtSoJ09yk0VL5ra4Y",
+
+    authDomain: "rexmarket-261c2.firebaseapp.com",
+
+    projectId: "rexmarket-261c2",
+
+    storageBucket: "rexmarket-261c2.firebasestorage.app",
+
+    messagingSenderId: "951626934305",
+
+    appId: "1:951626934305:web:af0513b9daf7b908949b60",
+
+    measurementId: "G-PH09V4MDW5"
+
+};
+
+
+// =========================================
+// INITIALIZE FIREBASE
+// =========================================
+
+const app = initializeApp(firebaseConfig);
+
+
+// =========================================
+// FIREBASE AUTHENTICATION
+// =========================================
+
+const auth = getAuth(app);
+
+
+// =========================================
+// FIRESTORE DATABASE
+// =========================================
+
+const db = getFirestore(app);
+
+
+// =========================================
+// EXPORT
+// =========================================
+
+export {
+    app,
+    auth,
+    db
+};
