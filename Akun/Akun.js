d98b567b5a4b81a53efd0a73ae3058b7c4c4814b
rexmarket <azilkhtr@gmail.com>
2026-09-17 14:32:43 +0700

    Implement Firebase connection alerts and event listeners
    Added Firebase connection alerts and button event listeners.

--- Akun/Akun.js
+++ Akun/Akun.js
@@ -1,1 +1,42 @@
-alert("Akun.js berhasil berjalan!");
+// =========================================
+// REXMARKET - FIREBASE TEST
+// =========================================
+
+import { auth, db } from "./Firebase.js";
+
+
+// =========================================
+// ELEMENT
+// =========================================
+
+const mainBtn = document.getElementById("mainBtn");
+const registerBtn = document.getElementById("registerBtn");
+
+
+// =========================================
+// FIREBASE TEST
+// =========================================
+
+console.log("REXMARKET FIREBASE BERHASIL TERHUBUNG");
+
+
+// =========================================
+// LOGIN BUTTON
+// =========================================
+
+mainBtn.addEventListener("click", () => {
+
+    alert("Firebase berhasil terhubung!");
+
+});
+
+
+// =========================================
+// REGISTER BUTTON
+// =========================================
+
+registerBtn.addEventListener("click", () => {
+
+    alert("Register siap dibuat!");
+
+});
